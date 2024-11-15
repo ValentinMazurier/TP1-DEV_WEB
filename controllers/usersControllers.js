18ff106b3e1d733f1bd2f19fc78e433883946a09
ValentinMazurier <valentin.mazu@gmail.com>
2024-11-15 10:26:28 +0100

    Ajout de la vérification de prénom et nom

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -12,10 +12,32 @@ exports.getAllUsers = function (req, res) {
 }
 
 
+
 exports.createnewUser = (req, res) => {
 	const { firstName, lastName } = req.body
 
+
+
+    // regex pour alphanumérique seulement
+function isAlphanumeric(str) {
+    const regex = /^[a-zA-Z0-9]+$/
+    return regex.test(str)
+}
+
     if(!firstName) return res.status(400).json({error: "The first name is required !"})
+
+    if (typeof firstName!=="string")
+        return res.status(400).json({error: "That's a weird name !"})
+
+    if (!isAlphanumeric(firstName))
+		return res.status(400).json({ error: "Ce nom n'est pas autorisé !" })
+
+    if (!firstName || !lastName)
+		return res
+			.status(400)
+			.json({ error: "Le prénom et le nom de famille sont requis !" })
+
+
 
         //run SQL query here
         db.run(
