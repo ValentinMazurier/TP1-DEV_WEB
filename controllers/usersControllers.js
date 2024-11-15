e813dc7b7e58a3dfc1270a577fdeab103151019d
ValentinMazurier <valentin.mazu@gmail.com>
2024-11-15 11:24:40 +0100

    Ajout de la méthode DELETE via SQL et suppression dans la base de données

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -11,6 +11,66 @@ exports.getAllUsers = function (req, res) {
 	})
 }
 
+exports.deleteUser = (req, res) => {
+	// get the id from the params
+	const { id } = req.params
+	// run the query
+	db.run("DELETE FROM users WHERE id = ?", [id], function (err) {
+		if (err) {
+			res.status(500).json({ error: err.message })
+		} else if (this.changes === 0) {
+			// if nothing found
+			res.status(404).json({ message: "User not found" })
+		} else {
+			// is successful
+			res.status(200).json({ message: "User deleted !" })
+		}
+	})
+}
+
+// POST mise à jour d'un utilisateur en fonction de son ID
+exports.updateUser = (req, res) => {
+	const { firstName, lastName } = req.body
+
+	// Récupérer l'id des paramètres
+	const userId = req.params.id
+
+	// Vérifier les champs envoyés
+	let updateFields = []
+	let queryParams = []
+
+	if (firstName) {
+		updateFields.push("firstName = ?")
+		queryParams.push(firstName)
+	}
+
+	if (lastName) {
+		updateFields.push("lastName = ?")
+		queryParams.push(lastName)
+	}
+
+	if (updateFields.length > 0) {
+		// Ajouter userId aux paramètres de la requête
+		queryParams.push(userId)
+
+		// Construire la requête dynamiquement
+		const query = `UPDATE users SET ${updateFields.join(", ")} WHERE id = ?`
+
+		db.run(query, queryParams, function (err) {
+			if (err) {
+				res.status(500).json({ error: err.message })
+			} else if (this.changes === 0) {
+				res.status(404).json({ message: "Utilisateur non trouvé" })
+			} else {
+				res.json({ msg: "Utilisateur mis à jour", userId, firstName, lastName })
+			}
+		})
+	} else {
+		res.status(400).json({ message: "Aucun champ à mettre à jour" })
+	}
+}
+
+
 
 
 exports.createnewUser = (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../database")
-const { getAllUsers, createnewUser } = require("../controllers/usersControllers")
+const { getAllUsers, createnewUser, updateUser, deleteUser } = require("../controllers/usersControllers")
 
 
 
@@ -12,44 +12,10 @@ router.get("/users", getAllUsers)
 router.post("/users", createnewUser);
 
 // PUT : modifier un utilisateur par ID
-router.put("/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const userIndex = users.findIndex((user) => user.id === id);
-
-	// Vérifie si l'utilisateur existe
-	if (userIndex < 0) {
-		return res.status(404).json({ msg: "utilisateur non trouvé" });
-	}
-
-	// Met à jour les informations de l'utilisateur si elles sont fournies
-	const { firstName, lastName } = req.body;
-
-	if (firstName) users[userIndex].firstName = firstName;
-	if (lastName) users[userIndex].lastName = lastName;
-
-	res.json({
-		msg: "utilisateur mis à jour",
-		user: users[userIndex],
-	});
-});
+router.put("/:id", updateUser);
 
 // DELETE : supprimer un utilisateur par ID
-router.delete("/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const userIndex = users.findIndex((user) => user.id === id);
-
-	// Vérifie si l'utilisateur existe
-	if (userIndex < 0) {
-		return res.status(404).json({ msg: "utilisateur non trouvé" });
-	}
-
-	// Supprime l'utilisateur de la liste
-	users.splice(userIndex, 1);
-
-	res.json({
-		msg: "utilisateur supprimé",
-	});
-});
+router.delete("/:id", deleteUser);
 
 // GET : lire un utilisateur par ID
 router.get("/:id", (req, res) => {
