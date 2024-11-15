8ccb15ff4559794912aa78a406a97084a727bad6
ValentinMazurier <valentin.mazu@gmail.com>
2024-11-15 09:07:09 +0100

    feat: Passage aux requêtes SQL pour récupérer les utilisateurs depuis la base de données
    - Ajout de la récupération des utilisateurs via une requête SQL au lieu d'utiliser une variable JavaScript usersArray.
    - Remplacement de es.json(usersArray) par db.all('SELECT * FROM users', [], callback) dans outes/users.js.
    - Explication : La requête SQL SELECT * FROM users permet de récupérer toutes les lignes de la table users. Le tableau vide [] est utilisé car il n'y a pas de valeurs paramétrées. La fonction de rappel gère les erreurs et renvoie les lignes récupérées.
    - Importation de db depuis ../database pour accéder à la connexion SQLite3.

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../database")
 
 
-const UsersArray = [
-	{ id: 1, firstName: "John", lastName: "Doe", role: "admin" },
-	{ id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
-	{ id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
-	{ id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
-	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
-];
+
+
 router.get("/users", (req, res) => {
-    res.json(UsersArray);
+    db.all('SELECT * FROM users', [], (err, rows) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.json(rows);
+        }
+      });
+    ;
 });
 
 // POST : créer un nouvel utilisateur
